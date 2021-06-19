3d27d27c7085cbc79dcba6b92da51dc25e94642e
Ange1D <47231898+Ange1D@users.noreply.github.com>
2021-06-19 10:29:29 -0500

    Update Proyecto.kt
    Creacion y asignacion de funciones

--- Proyecto.kt
+++ Proyecto.kt
@@ -1,1 +1,52 @@
+val tareas = mutableListOf<String>() 
+
+fun main() {
+  
+}
+
+fun bienvenida(){
+  var nombre 
+  val inputNombre = readLine()
+  print("Ingrese nombre:")
+  
+    print("Bienvenido $inputNombre Menu:
+          1 visualizar programa
+          2 revisar conflictos
+          3 ver resumen del usuario
+          4 Agregar tarea
+          5 Editar tarea
+          6 Eliminar tarea")
+    print("Opcion:")
+    val input = readLine()
+
+    when(input){
+      1 ->
+    
+    }
+}
+
+//JOSE
+fun visualizarPrograma(){
+}
+
+//BRAULIO
+fun revisarConflictos(){
+}
+
+//MANUEL
+fun resumenUsuario(){
+}
+
+//BRAULIO
+fun AgregarTarea(){
+}
+
+//ANGEL
+fun editarTarea(){
+}
+
+//JOSE
+fun eliminarTarea(){
+}
+
 
