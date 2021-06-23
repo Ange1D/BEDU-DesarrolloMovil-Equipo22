342f990bd9dbaf288727d5c3cd6f34294b9cdb90
Braulio David Hernández Palagot <72223472+Braulio-Palagot@users.noreply.github.com>
2021-06-23 01:00:19 -0500

    Update Proyecto.kt
    1) Se agregaron separaciones entre cada "pantalla" del programa con un encabezado con el fin de que el resultado en consola se vea más pulcro.
    2) Se agregaron detalles en las strings de los mensajes para el usuario con el fin de que el resultado en consola se vea más pulcro.
    3) Se agregó la función agregarTarea().
    4) Se agregó la función revisarConflictos().
    5) Se agregó una nueva lista mutable para manear los horarios de cada tarea, información que es necesaria para comprobar conflictos.

--- Proyecto.kt
+++ Proyecto.kt
@@ -1,82 +1,153 @@
-val tareas = mutableListOf<String>() 
+val tareas = mutableListOf<String>()
+val horariosTareas = mutableListOf<String>() //Lista mutable para guardar los horarios de cada tarea
 
 fun main() {
-  bienvenida()
+    bienvenida()
 }
 
-fun bienvenida(){
-  print("Ingrese nombre:")
-  val inputNombre = readLine()
-  
-    print("Bienvenido $inputNombre Menu \n1 visualizar programa \n2 revisar conflictos \n3 ver resumen del usuario \n4 Agregar tarea \n5 Editar tarea \n6 Eliminar tarea\n")
-    print("Seleccione una opcion:")
-    val input = readLine()
+fun bienvenida() {
+    var op = 0
+    print("Ingrese su nombre: ")
+    val inputNombre = readLine()
+    println("Bienvenido a tu agenda, $inputNombre.")
 
-    when(input?.toInt()){
-      1 -> visualizarPrograma()
-      2 -> revisarConflictos()
-      3 -> resumenUsuario()
-      4 -> AgregarTarea()
-      5 -> editarTarea()
-      6 -> eliminarTarea()
-      else -> {  print("opcion no valida")  } 
-    }
+    do {
+        print(
+            "---------------------------------------------\n" +
+                    "Menú principal:\n"
+        )
+        println(
+            "\t1) Visualizar programa.\n" +
+                    "\t2) Revisar conflictos.\n" +
+                    "\t3) Ver resumen del usuario.\n" +
+                    "\t4) Agregar tarea.\n" +
+                    "\t5) Editar tarea.\n" +
+                    "\t6) Eliminar tarea.\n" +
+                    "\t7) Cerrar Agenda."
+        )
+        print("Seleccione una opcion: ")
+        op = readLine()!!.toInt()
+
+        when (op) {
+            1 -> visualizarPrograma()
+            2 -> revisarConflictos()
+            3 -> resumenUsuario()
+            4 -> AgregarTarea()
+            5 -> editarTarea()
+            6 -> eliminarTarea()
+            7 -> print("Hasta la próxima $inputNombre.")
+            else -> {
+                print("opcion no valida.")
+            }
+        }
+    } while (op != 7)
 }
 
 //JOSE
-fun visualizarPrograma(){
-  if(tareas.isEmpty()){
-    println("No hay ninguna tarea")
-  }
-  else{
-    mostrarTareas()
-  }
+fun visualizarPrograma() {
+    println(
+        "---------------------------------------------\n" +
+                "Programa del día:\n"
+    )
+    if (tareas.isEmpty()) {
+        println("No hay ninguna tarea.")
+    } else {
+        mostrarTareas()
+    }
 }
 
 //BRAULIO
-fun revisarConflictos(){
+fun revisarConflictos() {
+    val setConflictos = mutableSetOf<String>()
+    println(
+        "---------------------------------------------\n" +
+                "Revisión de conflictos:\n"
+    )
+    for ((i, horarioComparando) in horariosTareas.withIndex()){
+        for ((j, horarioComparador) in horariosTareas.withIndex()) {
+            if (i != j) {
+                if (horarioComparando.contains(horarioComparador.substring(0, 5)) ||
+                    horarioComparando.contains(horarioComparador.substring(8)))
+                setConflictos.add("La tarea ${tareas[i]}, con horario: $horarioComparando\n" +
+                        "entra en conflicto con la tarea ${tareas[j]}, con horario: $horarioComparador.\n" +
+                        "Es necesario editar alguna de las dos.")
+            }
+        }
+    }
+    if (!setConflictos.isEmpty())
+        for ((i, elemento) in setConflictos.withIndex())
+            println("  ${i+1}) $elemento\n")
+    else
+        println("No se encontraron conflictos")
 }
 
 //MANUEL
-fun resumenUsuario(){
+fun resumenUsuario() {
+    println(
+        "---------------------------------------------\n" +
+                "Resumen del usuario:\n"
+    )
 }
 
 //BRAULIO
-fun AgregarTarea(){
+fun AgregarTarea() {
+    println(
+        "---------------------------------------------\n" +
+                "Agregar tarea:\n"
+    )
+    var horario = ""
+    print("Ingrese el título de la tarea: ");
+    var title = readLine().toString()
+    tareas.add(title)
+    print("\tIngrese la hora de inicio [HH:mm]: "); horario += readLine().toString()
+    horario += " - "
+    print("\tIngrese la hora de finalización [HH:mm]: "); horario += readLine().toString()
+    horariosTareas.add(horario)
 }
 
 //ANGEL
-fun editarTarea(){
-    if(tareas.isEmpty()){
-        println("No hay ninguna tarea")
-    }else{
-      mostrarTareas()
-        print("Numero de tarea a editar")
+fun editarTarea() {
+    var horario = ""
+    println(
+        "---------------------------------------------\n" +
+                "Editar tarea:\n"
+    )
+    if (tareas.isEmpty()) {
+        println("No hay ninguna tarea.")
+    } else {
+        mostrarTareas()
+        print("Numero de tarea a editar: ")
         val tareaIndice = readLine()
-        print("Escribe la nueva tarea")
+        print("Escribe el nuevo título para la tarea: ")
         val tareaEdit = readLine()
+        print("\tIngrese la hora de inicio [HH:mm]: "); horario += readLine().toString()
+        horario += " - "
+        print("\tIngrese la hora de finalización [HH:mm]: "); horario += readLine().toString()
+
         tareas.set(tareaIndice?.toInt()!!.minus(1), tareaEdit.toString())
-        println("Tarea editada")
+        horariosTareas.set(tareaIndice?.toInt()!!.minus(1), horario)
+        println("Tarea editada satisfactoriamente.")
     }
 }
 
 //JOSE
-fun eliminarTarea(){
-  if(tareas.isEmpty()){
-    println("No hay ninguna tarea")
-  }
-  else{
-    mostrarTareas()
-    print("Escriba el número de la tarea que desea borrar: ")
-    val delTarea = readLine()?.toInt()
-    tareas.removeAt(delTarea!!.minus(1))
-    mostrarTareas()
-  }
+fun eliminarTarea() {
+    println(
+        "---------------------------------------------\n" +
+                "Eliminar tarea:\n"
+    )
+    if (tareas.isEmpty()) {
+        println("No hay ninguna tarea")
+    } else {
+        mostrarTareas()
+        print("Escriba el número de la tarea que desea borrar: ")
+        val delTarea = readLine()?.toInt()
+        tareas.removeAt(delTarea!!.minus(1))
+        mostrarTareas()
+    }
 }
 
 //Nueva función para imprimir el arreglo completo de tareas
-fun mostrarTareas(){
-  for ((i, tarea) in tareas.withIndex()) println("${i + 1}.- $tarea")
+fun mostrarTareas() {
+    for ((i, tarea) in tareas.withIndex()) println("\t${i + 1}.- $tarea")
 }
-
-
