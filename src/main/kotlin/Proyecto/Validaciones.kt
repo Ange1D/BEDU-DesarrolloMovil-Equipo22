e796b6bcbf1ccdde3e7434145219846bc508f838
Ange1D <47231898+Ange1D@users.noreply.github.com>
2021-07-06 23:41:21 -0500

    Creacion de la clase validaciones

---
+++ src/main/kotlin/Proyecto/Validaciones.kt
@@ -0,0 +1,17 @@
+/**
+ * Copyright (c) 2021.
+ * @Author: Equipo 22.
+ * @members: JOSE BERNAL FONSECA; ANGEL OMAR GOMEZ CASTILLO; BRAULIO DAVID HERNANDEZ PALAGOT.
+ * @Topic: Desarrollo Móvil con Android: Kotlin Fundamentals.
+ * @Project: Proyecto Guía: ChronoMaster 2021.
+ */
+
+package Proyecto
+
+//Clase para validar los valores ingresados
+
+class Validaciones {
+
+
+
+}
