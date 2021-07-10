8e22e39ed2f77548ecfbb2b866c36383cfb56099
Braulio Hernández Palagot <bdpalagot@gmail.com>
2021-07-10 00:43:14 -0500

    Función enRango()
    Cambios realizados:
        1) Se agregó la función enRango() a la clase Validaciones. Esta función recibirá un valor mínimo y uno máximo para validar que el dato leído mediante la función numero() se encuentre en el rango requerido por la aplicación.

--- src/main/kotlin/Proyecto/Validaciones.kt
+++ src/main/kotlin/Proyecto/Validaciones.kt
@@ -9,11 +9,11 @@
 package Proyecto
 
 class Validaciones {
-    fun entero(Mensaje:String):Int{
+    fun entero(Mensaje: String): Int {
         return try {
             print("\t $Mensaje")
             //Si es null vuelve a llamar a la funcion
-            return Integer.valueOf(readLine()) ?: entero(Mensaje)
+            Integer.valueOf(readLine()) ?: entero(Mensaje)
         } catch (e: Exception) {
             //Si no es entero vulve a llamar a la funcion
             println("No es un numero valido")
@@ -21,10 +21,24 @@ class Validaciones {
         }
     }
 
-    fun texto(Mensaje:String):String{
-            print("\t $Mensaje")
-            //Si es null vuelve a llamar a la funcion
-            return readLine()?.ifBlank { null } ?: texto("\nNo puede dejar el campo vacio $Mensaje")
+    fun texto(Mensaje: String): String {
+        print("\t $Mensaje")
+        //Si es null vuelve a llamar a la funcion
+        return readLine()?.ifBlank { null } ?: texto("\nNo puede dejar el campo vacio\n\t $Mensaje")
 
+    }
+
+    fun enRango(Mensaje: String, minVal: Int, maxVal: Int): Int {
+        //Se realiza la comprobación de que se introduzca un entero
+        val lectura = entero(Mensaje)
+        var retorno = 0
+        //Se revisa que el entero leído se encuentre en el rango correcto
+        return if (lectura in minVal..maxVal)
+            lectura
+        else {
+            //Si no está en el rango correcto, se vuelve a llamar a la función
+            println("El dato no se encuentra en el rango establecido.")
+            enRango(Mensaje, minVal, maxVal)
+        }
     }
 }
